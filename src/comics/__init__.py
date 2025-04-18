760e12dba16d5308f2039c553b57c6e49514c16f
Ira Horecka <ira89@icloud.com>
2025-04-18 09:15:31 -0400

    fix pyproject.toml source directory search space - update pypi version to 0.5.2

--- src/comics/__init__.py
+++ src/comics/__init__.py
@@ -7,4 +7,4 @@ from comics.constants import directory
 from comics.exceptions import InvalidDateError, InvalidEndpointError
 from comics.gocomics import search
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
