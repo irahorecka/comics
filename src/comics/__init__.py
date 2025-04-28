72501fd6b45ff586c41dac81f3091dab9359838e
Ira Horecka <ira89@icloud.com>
2025-04-28 16:09:59 -0400

    update pypi version to 0.5.5

--- src/comics/__init__.py
+++ src/comics/__init__.py
@@ -7,4 +7,4 @@ from comics.constants import directory
 from comics.exceptions import InvalidDateError, InvalidEndpointError
 from comics.gocomics import search
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
